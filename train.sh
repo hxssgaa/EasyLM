1591bdf01ec166e22bc52e3123622c8f5571f87d
David Huang <hxssg1124@gmail.com>
2023-11-17 21:09:07 +0800

    fix

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ export WANDB_API_KEY='9f081bf8abc9f49dffeb68c6cf978320514ab4b5'
 
 WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_train \
     --total_steps=150000 \
-    --mesh_dim='!1,8,-1' \
+    --mesh_dim='1,8,-1' \
     --log_freq=100 \
     --save_model_freq=1000 \
     --load_llama_config='7b' \
@@ -19,7 +19,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_
     --train_dataset.huggingface_dataset.seq_length=2048 \
     --train_dataset.huggingface_dataset.batch_size=256 \
     --logger.output_dir='gs://hxtpu_bucket/llama2_mc4' \
-    --logger.online=True \
+    --logger.online=False \
     --logger.prefix='EasyLM' \
     --logger.project="my_llama2_7b" \
     --dtype=bf16 \
