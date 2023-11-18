7113528c4d90af54968c32ddda540393fdaa7eee
David Huang <hxssg1124@gmail.com>
2023-11-18 15:08:03 +0800

    fix

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ export WANDB_API_KEY='9f081bf8abc9f49dffeb68c6cf978320514ab4b5'
 
 WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_train \
     --total_steps=150000 \
-    --mesh_dim='1,8,-1' \
+    --mesh_dim='1,16,-1' \
     --log_freq=100 \
     --save_model_freq=1000 \
     --load_llama_config='7b' \
@@ -18,7 +18,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_
     --train_dataset.huggingface_dataset.name='en' \
     --train_dataset.huggingface_dataset.streaming=True \
     --train_dataset.huggingface_dataset.seq_length=4096 \
-    --train_dataset.huggingface_dataset.batch_size=128 \
+    --train_dataset.huggingface_dataset.batch_size=64 \
     --logger.output_dir='gs://hxtpu_bucket/llama2_mc4' \
     --logger.online=False \
     --logger.prefix='EasyLM' \
@@ -26,7 +26,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_
     --dtype=bf16 \
     --optimizer.adamw_optimizer.lr=1e-4 \
     --optimizer.adamw_optimizer.end_lr=5e-5 \
-    --optimizer.accumulate_gradient_steps=16 \
+    --optimizer.accumulate_gradient_steps=32 \
     --optimizer.adamw_optimizer.lr_warmup_steps=2000 \
     --optimizer.adamw_optimizer.lr_decay_steps=150000 \
     --optimizer.adamw_optimizer.bf16_momentum=True \
