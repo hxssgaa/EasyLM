12d144c3e22a2a26096359e717ca1d93d7567e37
David Huang <hxssg1124@gmail.com>
2023-11-19 10:24:53 +0800

    fix

--- EasyLM/flash_attn_utils.py
+++ EasyLM/flash_attn_utils.py
@@ -23,7 +23,7 @@ def mha_reference(
     """Reference multi-headed attention implementation."""
     # We apply the scale factor before the attention biases.
     q *= softmax_scale
-    logits = jnp.einsum("btnh,bsnh->bnts", q, k)
+    logits = jnp.einsum("bnth,bnsh->btns", q, k)
 
     if bias is not None:
         logits += bias.astype(logits.dtype)
