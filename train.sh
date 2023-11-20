8efac8416d95742c53197a85467a3a2f54e270b9
David Huang <hxssg1124@gmail.com>
2023-11-20 10:40:48 +0800

    fix

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ export WANDB_API_KEY='9f081bf8abc9f49dffeb68c6cf978320514ab4b5'
 WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_train \
     --total_steps=150000 \
     --mesh_dim='1,16,-1' \
-    --log_freq=10 \
+    --log_freq=50 \
     --save_model_freq=1000 \
     --load_llama_config='7b' \
     --tokenizer.vocab_file='gs://hxtpu_bucket/llama2_tokenizer.model' \
