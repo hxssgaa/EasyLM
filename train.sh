b0dd09a10415ae232f375dc355c839ad5d932e61
David Huang <hxssg1124@gmail.com>
2023-11-20 10:26:14 +0800

    fix

--- train.sh
+++ train.sh
@@ -9,6 +9,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_
     --save_model_freq=1000 \
     --load_llama_config='7b' \
     --tokenizer.vocab_file='gs://hxtpu_bucket/llama2_tokenizer.model' \
+    --load_checkpoint='params::gs://hxtpu_bucket/llama2_7b_easylm' \
     --llama.max_sequence_length=4096 \
     --train_dataset.text_processor.fields="text" \
     --train_dataset.type=huggingface \
