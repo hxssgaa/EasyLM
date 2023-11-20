71b882c66ade4f706c8dd6326efd8f3259dd3a9e
David Huang <hxssg1124@gmail.com>
2023-11-20 21:49:06 +0800

    use flash

--- EasyLM/models/llama/llama_model.py
+++ EasyLM/models/llama/llama_model.py
@@ -192,7 +192,7 @@ class LLaMAConfig(PretrainedConfig):
         remat_attention='',
         remat_mlp='',
         scan_attention=False,
-        flash_attention=False,
+        flash_attention=True,
         scan_mlp=False,
         scan_query_chunk_size=512,
         scan_key_chunk_size=512,
