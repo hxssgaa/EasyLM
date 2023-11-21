19ff01108019030962ef07db5c0641cef3a082eb
David Huang <hxssg1124@gmail.com>
2023-11-21 13:20:33 +0800

    online

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ export WANDB_API_KEY='9f081bf8abc9f49dffeb68c6cf978320514ab4b5'
 
 # export LIBTPU_INIT_ARGS='--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000 --xla_tpu_spmd_rewrite_einsum_with_reshape=true --xla_enable_async_all_gather=true --xla_tpu_enable_latency_hiding_scheduler=true TPU_MEGACORE=MEGACORE_DENSE'
 
-WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_train \
+WANDB__SERVICE_WAIT=300 WANDB_MODE=online python3 -m EasyLM.models.llama.llama_train \
     --total_steps=150000 \
     --mesh_dim='1,16,-1' \
     --log_freq=50 \
@@ -19,7 +19,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.llama.llama_
     --train_dataset.huggingface_dataset.seq_length=4096 \
     --train_dataset.huggingface_dataset.batch_size=256 \
     --logger.output_dir='gs://hxtpu_bucket/llama2_mc4' \
-    --logger.online=False \
+    --logger.online=True \
     --logger.prefix='EasyLM' \
     --logger.project="my_llama2_7b" \
     --dtype=bf16 \
