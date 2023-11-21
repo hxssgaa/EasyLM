70af12be921fcaacc7a38716dc4ae93cdeedce3a
David Huang <hxssg1124@gmail.com>
2023-11-21 19:44:25 +0800

    fix

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ export WANDB_API_KEY='9f081bf8abc9f49dffeb68c6cf978320514ab4b5'
 WANDB__SERVICE_WAIT=300 WANDB_MODE=online python3 -m EasyLM.models.llama.llama_train \
     --total_steps=150000 \
     --mesh_dim='1,16,-1' \
-    --log_freq=50 \
+    --log_freq=100 \
     --save_model_freq=1000 \
     --load_llama_config='7b' \
     --tokenizer.vocab_file='gs://hxtpu_bucket/llama2_tokenizer.model' \
@@ -13,8 +13,8 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=online python3 -m EasyLM.models.llama.llama_t
     --llama.max_sequence_length=4096 \
     --train_dataset.text_processor.fields="text" \
     --train_dataset.type=huggingface \
-    --train_dataset.huggingface_dataset.path='mc4' \
-    --train_dataset.huggingface_dataset.name='en' \
+    --train_dataset.huggingface_dataset.path='togethercomputer/RedPajama-Data-V2' \
+    --train_dataset.huggingface_dataset.name='default' \
     --train_dataset.huggingface_dataset.streaming=True \
     --train_dataset.huggingface_dataset.seq_length=4096 \
     --train_dataset.huggingface_dataset.batch_size=256 \
@@ -23,8 +23,8 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=online python3 -m EasyLM.models.llama.llama_t
     --logger.prefix='EasyLM' \
     --logger.project="my_llama2_7b" \
     --dtype=bf16 \
-    --optimizer.adamw_optimizer.lr=1e-4 \
-    --optimizer.adamw_optimizer.end_lr=5e-5 \
+    --optimizer.adamw_optimizer.lr=5e-5 \
+    --optimizer.adamw_optimizer.end_lr=1e-5 \
     --optimizer.accumulate_gradient_steps=1 \
     --optimizer.adamw_optimizer.lr_warmup_steps=200 \
     --optimizer.adamw_optimizer.lr_decay_steps=150000 \
