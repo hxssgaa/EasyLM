bfb353659527f2d6815f371c081abec9e0a2fe2d
David Huang <hxssg1124@gmail.com>
2023-11-21 20:14:14 +0800

    commit

--- train.sh
+++ train.sh
@@ -11,7 +11,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=online python3 -m EasyLM.models.llama.llama_t
     --tokenizer.vocab_file='gs://hxtpu_bucket/llama2_tokenizer.model' \
     --load_checkpoint='params::gs://hxtpu_bucket/llama2_7b_easylm' \
     --llama.max_sequence_length=4096 \
-    --train_dataset.text_processor.fields="text" \
+    --train_dataset.text_processor.fields="raw_content" \
     --train_dataset.type=huggingface \
     --train_dataset.huggingface_dataset.path='togethercomputer/RedPajama-Data-V2' \
     --train_dataset.huggingface_dataset.name='default' \
