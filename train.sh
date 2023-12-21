87d1907a3981704ab2df2e68cec5d311123d99c8
David Huang <hxssg1124@gmail.com>
2023-12-21 18:15:39 +0800

    fix

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ export WANDB_API_KEY='9f081bf8abc9f49dffeb68c6cf978320514ab4b5'
 # export LIBTPU_INIT_ARGS='--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000 --xla_tpu_spmd_rewrite_einsum_with_reshape=true --xla_enable_async_all_gather=true --xla_tpu_enable_latency_hiding_scheduler=true TPU_MEGACORE=MEGACORE_DENSE'
     # --load_checkpoint='params::gs://hxtpu_bucket/mistral_sea_7b_easylm' \
     # --tokenizer.vocab_file='gs://hxtpu_bucket/chinese_mistral_tokenizer.model' \
-WANDB__SERVICE_WAIT=300 WANDB_MODE=online python3 -m EasyLM.models.mistral.mistral_train \
+WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.mistral.mistral_train \
     --total_steps=150000 \
     --mesh_dim='1,16,-1' \
     --log_freq=1 \
@@ -25,6 +25,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=online python3 -m EasyLM.models.mistral.mistr
     --dtype=bf16 \
     --optimizer.adamw_optimizer.lr=5e-5 \
     --optimizer.adamw_optimizer.end_lr=1e-5 \
+    --optimizer.adamw_optimizer.b2=0.999 \
     --optimizer.accumulate_gradient_steps=32 \
     --optimizer.adamw_optimizer.lr_warmup_steps=4000 \
     --optimizer.adamw_optimizer.lr_decay_steps=150000 \
