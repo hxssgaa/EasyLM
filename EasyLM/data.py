2e7fa44a0440fb62922b72eef0f6b7e6c900cf4d
David Huang <hxssg1124@gmail.com>
2023-12-21 17:14:05 +0800

    debug

--- EasyLM/data.py
+++ EasyLM/data.py
@@ -230,7 +230,6 @@ class HuggingfaceDataset(object):
                             self.config.batch_size, -1
                         ),
                     }
-                    import pdb; pdb.set_trace()
                     if self.config.always_start_with_bos:
                         batch['input_tokens'][:, 0] = self.tokenizer.bos_token_id
                     yield batch, metrics
@@ -398,6 +397,7 @@ class JsonDataset(object):
                         self.config.batch_size, -1
                     ),
                 }
+                import pdb; pdb.set_trace()
                 if self.config.always_start_with_bos:
                     batch['input_tokens'][:, 0] = self.tokenizer.bos_token_id
                 yield batch, metrics
