62cd0d4951c9659282724a00009fab71e0ff12cd
David Huang <hxssg1124@gmail.com>
2023-12-21 16:53:41 +0800

    debug

--- EasyLM/data.py
+++ EasyLM/data.py
@@ -230,6 +230,7 @@ class HuggingfaceDataset(object):
                             self.config.batch_size, -1
                         ),
                     }
+                    import pdb; pdb.set_trace()
                     if self.config.always_start_with_bos:
                         batch['input_tokens'][:, 0] = self.tokenizer.bos_token_id
                     yield batch, metrics
