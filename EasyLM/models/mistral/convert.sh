3d5111f52dc2c4910db4769c9afe01b566726a73
David Huang <hxssg1124@gmail.com>
2023-12-21 23:45:19 +0800

    fix mistral pretraining

---
+++ EasyLM/models/mistral/convert.sh
@@ -0,0 +1,5 @@
+python convert_hf_to_easylm.py  \
+       --checkpoint_dir    /home/hxssgaa/Developer/seallama2/Mistral-SEA-7B-v0.1-2    \
+       --output_file ./sea_mistral_easylm   \
+       --model_size 7b \
+       --streaming
