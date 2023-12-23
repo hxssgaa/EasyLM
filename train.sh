b1bc3a6b7a9d3c590c25dd08ba1f308d80a2598c
David Huang <hxssg1124@gmail.com>
2023-12-23 18:28:56 +0800

    fix

--- train.sh
+++ train.sh
@@ -30,7 +30,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.mistral.mist
     --optimizer.adamw_optimizer.end_lr=1e-5 \
     --optimizer.adamw_optimizer.b2=0.999 \
     --optimizer.accumulate_gradient_steps=32 \
-    --optimizer.adamw_optimizer.lr_warmup_steps=4000 \
+    --optimizer.adamw_optimizer.lr_warmup_steps=2048 \
     --optimizer.adamw_optimizer.lr_decay_steps=150000 \
     --optimizer.adamw_optimizer.bf16_momentum=True \
     --checkpointer.save_optimizer_state=True \
