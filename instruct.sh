be8507f44e0115e995dc202e6ef6737681885d30
Xin Huang <hxssg1124@gmail.com>
2024-01-29 00:47:10 +0800

    fix instruct

--- instruct.sh
+++ instruct.sh
@@ -4,11 +4,11 @@ export WANDB_API_KEY='9f081bf8abc9f49dffeb68c6cf978320514ab4b5'
     # --load_checkpoint='params::gs://hxtpu_bucket/mistral_sea_7b_easylm' \
     # --tokenizer.vocab_file='gs://hxtpu_bucket/chinese_mistral_tokenizer.model' \
 WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.mistral.mistral_train \
-    --total_steps=20480 \
+    --total_steps=5120 \
     --mesh_dim='1,16,-1'\
     --log_freq=256 \
     --save_model_freq=512 \
-    --save_milestone_freq=10240 \
+    --save_milestone_freq=2560 \
     --load_mistral_config='7b' \
     --tokenizer.vocab_file='gs://hxtpu_bucket/chinese_mistral_tokenizer.model' \
     --load_checkpoint='trainstate_params::gs://hxtpu_bucket/sea_mistral_7b_outputs/mix_sea_mc/streaming_train_state' \
