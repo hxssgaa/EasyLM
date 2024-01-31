5936655b541ecc767757312c68e92135baf87de3
Xin Huang <hxssg1124@gmail.com>
2024-01-31 10:38:14 +0800

    fix for inference

--- inference.sh
+++ inference.sh
@@ -1,6 +1,6 @@
 python -m EasyLM.models.mistral.mistral_serve \
     --load_mistral_config='7b' \
-    --load_checkpoint="trainstate_params::gs://hxtpu_bucket/sea_mistral_7b_outputs/mix_sea_mc/streaming_train_state" \
+    --load_checkpoint="trainstate_params::gs://hxtpu_bucket/sea_mistral_7b_inst_outputs/EasyLM-instruct_0129--mix_sea_mc/streaming_train_state" \
     --tokenizer.vocab_file='gs://hxtpu_bucket/chinese_mistral_tokenizer.model' \
     --mesh_dim='1,-1,1' \
     --dtype='bf16' \
