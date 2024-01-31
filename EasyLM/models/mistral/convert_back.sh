ffc4aaf29ce7420978415f8c232015dee5be1edf
Xin Huang <hxssg1124@gmail.com>
2024-01-31 10:35:38 +0800

    convert

--- EasyLM/models/mistral/convert_back.sh
+++ EasyLM/models/mistral/convert_back.sh
@@ -1,5 +1,5 @@
 python convert_easylm_to_hf.py  \
-       --load_checkpoint="trainstate_params::gs://hxtpu_bucket/sea_mistral_7b_outputs/mix_sea_mc/streaming_train_state"    \
-       --output_dir="./sea_mistral_7b"   \
+       --load_checkpoint="trainstate_params::gs://hxtpu_bucket/sea_mistral_7b_inst_outputs/EasyLM-instruct_0129--mix_sea_mc/streaming_train_state"    \
+       --output_dir="./sea_mistral_inst_7b"   \
        --tokenizer_path="./chinese_mistral_tokenizer.model" \
        --model_size="7b"
