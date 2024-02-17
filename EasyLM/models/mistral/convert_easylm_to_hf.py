d346cb17741fd4bab6042aeb25516d5988e59c76
Xin Huang <hxssg1124@gmail.com>
2024-02-17 09:16:43 +0800

    fix

--- EasyLM/models/mistral/convert_easylm_to_hf.py
+++ EasyLM/models/mistral/convert_easylm_to_hf.py
@@ -118,7 +118,6 @@ def write_model(loaded, model_path, model_size):
         filename = f"pytorch_model-{layer_i + 1}-of-{n_layers + 1}.bin"
         if FLAGS.enable_lora:
             for k in ['wq', 'wk', 'wv', 'wo']:
-                import pdb; pdb.set_trace()
                 loaded[f"transformer.h.{layer_i}.attention.%s.kernel" % k] += (loaded[f"transformer.h.{layer_i}.attention.%s.lora_B" % k] @ loaded[f"transformer.h.{layer_i}.attention.%s.lora_A" % k]).T * scaling
             for k in ['w1', 'w2', 'w3']:
                 loaded[f"transformer.h.{layer_i}.feed_forward.%s.kernel" % k] += (loaded[f"transformer.h.{layer_i}.feed_forward.%s.lora_B" % k] @ loaded[f"transformer.h.{layer_i}.feed_forward.%s.lora_A" % k]).T * scaling
