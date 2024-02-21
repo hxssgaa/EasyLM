c1038ad22d7b8fca98f60376b2dec6abf3d2a6d3
Huang Xin <hxssg1124@gmail.com>
2024-02-21 20:30:20 +0800

    fix

--- EasyLM/models/mistral/convert_back.sh
+++ EasyLM/models/mistral/convert_back.sh
@@ -1,5 +1,6 @@
 python convert_easylm_to_hf.py  \
-       --load_checkpoint="trainstate_params::gs://hxtpu_bucket/sea_mistral_7b_inst_outputs/EasyLM-instruct_0129--mix_sea_mc/streaming_train_state"    \
+       --load_checkpoint="trainstate_params::gs://hxtpu_bucket/sea_mistral_7b_star_inst_outputs/EasyLM-replay_lora_openhermes2_4--mix_sea_mc/streaming_train_state_15360"    \
        --output_dir="./sea_mistral_inst_7b"   \
        --tokenizer_path="./chinese_mistral_tokenizer.model" \
+       --enable_lora=True \
        --model_size="7b"
