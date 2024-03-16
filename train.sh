b2ae2d10d2c6c7c5e543f3c2b655a7e9f11e18ab
Xin Huang <hxssg1124@gmail.com>
2024-03-16 16:21:58 +0800

    fix

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ WANDB__SERVICE_WAIT=300 WANDB_MODE=offline python3 -m EasyLM.models.mistral.mist
     --total_steps=128000 \
     --mesh_dim='!1,32,2'\
     --log_freq=512 \
-    --eval_steps=4 \
+    --eval_steps=0 \
     --save_model_freq=512 \
     --save_milestone_freq=10240 \
     --save_best=True \
